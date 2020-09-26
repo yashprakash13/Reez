f410a9b35abc0b322263653096be4a883121e978
yashprakash13 <yashprakash13@gmail.com>
2020-09-26 23:16:55 +0530

    added nav graph, activity main xml

--- app/src/main/java/com/regency/app/reez/MainActivity.kt
+++ app/src/main/java/com/regency/app/reez/MainActivity.kt
@@ -2,10 +2,14 @@ package com.regency.app.reez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.regency.app.reez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
